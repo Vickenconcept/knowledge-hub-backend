dbda862b216c39b47c2fbf129a0488f58b9f645c
William Victor <vicken408@gmail.com>
2025-10-22 14:10:54 +0100

    uniformit

--- routes/api.php
+++ routes/api.php
@@ -110,7 +110,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('orgs/{org}/connectors', [ConnectorController::class, 'index'])->middleware('throttle:60,1'); // 60 requests per minute
     Route::post('orgs/{org}/connectors', [ConnectorController::class, 'create'])->middleware('throttle:10,1'); // 10 creates per minute
     Route::post('connectors/{id}/oauth/callback', [ConnectorController::class, 'oauthCallback'])->middleware('throttle:20,1'); // 20 callbacks per minute
-    Route::post('connectors/{id}/start-ingest', [ConnectorController::class, 'startIngest'])->middleware('throttle:20,1'); // 5 ingests per minute
+    Route::post('connectors/{id}/start-ingest', [ConnectorController::class, 'startIngest'])->middleware('throttle:1,1'); // 5 ingests per minute
     Route::post('connectors/{id}/stop-sync', [ConnectorController::class, 'stopSync'])->middleware('throttle:10,1'); // 10 stops per minute
     Route::post('connectors/{id}/disconnect', [ConnectorController::class, 'disconnect'])->middleware('throttle:10,1'); // 10 disconnects per minute
     Route::get('connectors/{connectorId}/job-status', [ConnectorController::class, 'getJobStatus'])->middleware('throttle:120,1'); // 120 status checks per minute
