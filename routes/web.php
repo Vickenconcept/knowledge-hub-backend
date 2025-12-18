68afb6c4700a22a408483685a14b2df4ee96c1de
victor william <111221295+Vickenconcept@users.noreply.github.com>
2025-12-18 09:01:52 +0100

    Comment out the root route in web.php
    Comment out the welcome route for future reference.

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 
